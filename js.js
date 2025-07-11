fdfd83326c1b359943a38659cbf13c1576f6fa37
abdullohsoatov001 <soatovabdulloh541@gmail.com>
2025-07-11 11:57:37 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,53 @@
+// let soz = "hello"
+// let harf = ""
+
+// for (let i = 0; i < soz.length; i++) {
+//     harf += soz[i] + "oi"
+// }
+// console.log(harf);
+
+let son = [-12, 20, -14, 0, 25, 2]
+let son1 = []
+
+for (let i = 0; i < son.length; i++) {
+    if (son[i] < 0) {
+        son1.push(son[i])
+
+    }
+}
+
+// console.log(son1);
+
+// 1. Arraydagi eng uzun stringni toping
+// Tavsif: Berilgan array ichidan eng uzun string qiymatni toping.
+// input: ["apple", "banana", "kiwi", "watermelon"]
+// output: "watermelon"
+
+// let sozlar = ["apple", "banana", "kiwi", "watermelon"];
+// let engUzun = "";
+
+// for (let i = 0; i < sozlar.length; i++) {
+//     if (sozlar[i].length > engUzun.length) {
+//         engUzun = sozlar[i];
+//     }
+// }
+// console.log("Eng uzun so'z:", engUzun);
+
+// ✅ 2. String palindrome ekanligini aniqlang
+// Tavsif: Foydalanuvchi kiritgan so‘z o‘zini teskari o‘qishda ham bir xil bo‘lsa, true qaytaring.
+
+// let sozlar = ["apple", "hello"]
+// let soz = ""
+
+// for (let i = 0; i < sozlar.length; i++) {
+//     soz +=  
+//     console.log(soz);
+
+// }
+
+
+
+
+
+
+
